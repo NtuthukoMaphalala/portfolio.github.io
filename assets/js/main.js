5a97fe03dd215680d1b054f5cfae5a605907dd7f
Ntuthuko Maphalala <82970357+NtuthukoMaphalala@users.noreply.github.com>
2024-10-25 10:57:24 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
+const correctPassword = '6111'; // Set your password here
+
+function promptPassword() {
+    const password = prompt('Please enter the password to reveal the ID:');
+    if (password === correctPassword) {
+        showId();
+    } else {
+        alert('Incorrect password. Please try again.');
+    }
+}
+
+function showId() {
+    var userIdElement = document.getElementById('userId');
+    userIdElement.style.display = "inline";
+    userIdElement.previousSibling.style.display = "none"; // hide the placeholder
+}
+
 (function () {
   "use strict";
 
